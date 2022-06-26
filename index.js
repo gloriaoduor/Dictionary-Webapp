8ac19ef670d3aad10d07118c8076d8bc89698b2f
gloria oduor <gloriaoduor@gmail.com>
2022-06-26 12:28:23 +0300

    editted index.js

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-const pageContent = document.querySelector(".page-content"),
-searchInput = pageContent.querySelector("input"),
+document.addEventListener('DOMContentLoaded', ()=>{
 
-infoText = pageContent.querySelector(".info-text"),
+    const pageContent = document.querySelector(".page-content"),
+    searchInput = pageContent.querySelector("input"),
 
-removeIcon = pageContent.querySelector(".search span");
-resetBtn = pageContent.querySelector('.btn')
+    infoText = pageContent.querySelector(".info-text"),
 
-//function to fetch data from the API
-function fetchApi(word){
-    pageContent.classList.remove("active");
-    infoText.style.color = "#000";
-    infoText.innerHTML = 'Searching...';
-    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url)
-    .then(response => response.json())
-    .then(result => data(result, word))
-    .catch(() =>{
-        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Check your spelling or try another word`;
-    });
-}
+    removeIcon = pageContent.querySelector(".search span");
+    resetBtn = pageContent.querySelector('.btn')
 
-//function to search for the word
-function search(word){
-    fetchApi(word);
-    searchInput.value = word;
-}
-
-//function to display the result of the query 
-function data(result, word){
-    if(result.title){
-        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Check your spelling or try another word`;
-    }else{
-        pageContent.classList.add("active");
-        let definitions = result[0].meanings[0].definitions[0];
-        document.querySelector(".word p").innerText = result[0].word;
-        document.querySelector(".meaning span").innerText = definitions.definition;
+    //function to fetch data from the API
+    function fetchApi(word){
+        pageContent.classList.remove("active");
+        infoText.style.color = "#000";
+        infoText.innerHTML = 'Searching...';
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(result => data(result, word))
+        .catch(() =>{
+            infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Check your spelling or try another word`;
+        });
     }
-}
-//Events
-searchInput.addEventListener("keyup", e =>{
-    let word = e.target.value.replace(/\s+/g, ' ');
-    if(e.key == "Enter" && word){
+
+    //function to search for the word
+    function search(word){
         fetchApi(word);
+        searchInput.value = word;
     }
-});
 
-removeIcon.addEventListener("click", ()=>{
-    searchInput.value = "";
-    searchInput.focus();
-    pageContent.classList.remove("active");
-    infoText.style.color = "#9A9A9A";
-    infoText.innerHTML = "Type any existing word and press enter to get meaning";
-});
+    //function to display the result of the query 
+    function data(result, word){
+        if(result.title){
+            infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Check your spelling or try another word`;
+        }else{
+            pageContent.classList.add("active");
+            let definitions = result[0].meanings[0].definitions[0];
+            document.querySelector(".word p").innerText = result[0].word;
+            document.querySelector(".meaning span").innerText = definitions.definition;
+        }
+    }
+    //Events
+    searchInput.addEventListener("keyup", e =>{
+        let word = e.target.value.replace(/\s+/g, ' ');
+        if(e.key == "Enter" && word){
+            fetchApi(word);
+        }
+    });
 
-resetBtn.addEventListener('click', (e)=>{
-    pageContent.classList.remove("active");
-    infoText.innerHTML = "";
-    searchInput.value = "";
+    removeIcon.addEventListener("click", ()=>{
+        searchInput.value = "";
+        searchInput.focus();
+        pageContent.classList.remove("active");
+        infoText.style.color = "#9A9A9A";
+        infoText.innerHTML = "Type any existing word and press enter to get meaning";
+    });
 
+    resetBtn.addEventListener('click', (e)=>{
+        pageContent.classList.remove("active");
+        infoText.innerHTML = "";
+        searchInput.value = "";
+
+    })
 })
